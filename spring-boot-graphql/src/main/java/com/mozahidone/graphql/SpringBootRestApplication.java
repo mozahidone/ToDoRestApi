f7d7112b691377fd4a23b14b62cda87493d84908
mozahid <mozahidone@gmail.com>
2023-04-26 17:47:33 +0600

    Code refactoring

--- spring-boot-graphql/src/main/java/com/mozahidone/graphql/SpringBootRestApplication.java
+++ spring-boot-graphql/src/main/java/com/mozahidone/graphql/SpringBootRestApplication.java
@@ -3,7 +3,7 @@ package com.mozahidone.graphql;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.demo.api"})
+@SpringBootApplication(scanBasePackages = {"com.mozahidone.graphql"})
 public class SpringBootRestApplication {
 
 	public static void main(String[] args) {
