42f4d637b0475dc35385cb856675c27524735286
mozahid <mozahidone@gmail.com>
2023-05-04 19:37:26 +0600

    JWT implementation using spring security in spring boot

---
+++ spring-security-jwt/src/main/java/com/mozahidone/jwt/api/entity/AuthRequest.java
@@ -0,0 +1,14 @@
+package com.mozahidone.jwt.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+
+    private String userName;
+    private String password;
+}
