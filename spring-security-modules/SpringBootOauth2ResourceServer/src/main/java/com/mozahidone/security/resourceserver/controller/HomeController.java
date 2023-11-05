b1da68385aeb750853830292006369e19b1ce4e1
mozahidone <mozahidone@gmail.com>
2023-11-05 11:16:46 +0600

    Spring Boot Oauth2 resource server module added

---
+++ spring-security-modules/SpringBootOauth2ResourceServer/src/main/java/com/mozahidone/security/resourceserver/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.mozahidone.security.resourceserver.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		LocalDateTime time = LocalDateTime.now();
+		return "Welcome Home from First Server! - " + time;
+	}
+
+}
